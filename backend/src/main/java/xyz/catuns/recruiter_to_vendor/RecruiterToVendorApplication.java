8f32489296de5e480069bf244112632b1e8fcc4f
Devin Catuns <devincatuns1@gmail.com>
2025-06-09 11:19:10 -0400

    created backend application

---
+++ backend/src/main/java/xyz/catuns/recruiter_to_vendor/RecruiterToVendorApplication.java
@@ -0,0 +1,13 @@
+package xyz.catuns.recruiter_to_vendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruiterToVendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruiterToVendorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/xyz/catuns/recruiter_to_vendor/RecruiterToVendorApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.catuns.recruiter_to_vendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruiterToVendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
