33dcca80f615aaba49a6f67db9fee18459c69256
Devin Catuns <devincatuns1@gmail.com>
2025-06-11 15:15:30 -0400

    add properties for profiles/environments

--- backend/src/main/java/xyz/catuns/recruiter_to_vendor/entities/Candidate.java
+++ backend/src/main/java/xyz/catuns/recruiter_to_vendor/entities/Candidate.java
@@ -10,9 +10,12 @@ import java.util.List;
 public class Candidate {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
     private String jobTitle;
 
     @OneToOne(cascade = CascadeType.ALL)
@@ -29,22 +32,6 @@ public class Candidate {
 
     @OneToMany(cascade = CascadeType.ALL)
     private final List<Skill> skills = new ArrayList<>();
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getJobTitle() {
         return jobTitle;
